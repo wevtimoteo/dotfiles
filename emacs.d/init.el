87f1bca4cee79f6356e42a7558a112c198874924
Weverton do Couto Timoteo <weverton@wolcan.us>
2014-03-16 00:42:27 -0300

    Add emacs packages: cider, clojure-mode, starter-kit, etc

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,3 +2,15 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      clojure-mode
+                      clojure-test-mode
+                      cider))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
