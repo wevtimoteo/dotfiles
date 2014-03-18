998927cf19d5de58ca92cb7e110a8c64a01af99b
Weverton do Couto Timoteo <weverton@wolcan.us>
2014-03-18 11:18:48 -0300

    Add dot emacs file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc awsrc tmux.conf )
+files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc awsrc tmux.conf emacs )
 
 for filename in ${files[@]}
 do
@@ -9,6 +9,6 @@ done
 ln -fs $PWD/git-edit /usr/local/bin/git-edit
 ln -fs $PWD/capssh /usr/local/bin/capssh
 ln -fs $PWD/capcssh /usr/local/bin/capcssh
-ln -s $PWD/emacs.d ~/.emacs.d
+ln -fs $PWD/emacs.d/ ~/.emacs.d/
 
 source ~/.bash_profile
