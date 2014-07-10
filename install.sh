7313708fd159bd3fc6448250af9a5c2c80907cde
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2014-07-10 15:37:41 -0300

    Add pryrc

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc awsrc tmux.conf emacs )
+files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc awsrc tmux.conf emacs pryrc )
 
 for filename in ${files[@]}
 do
