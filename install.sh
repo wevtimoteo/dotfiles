d96a4cde9612481bc2aa3499cc9507eec30079f7
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2014-10-14 13:57:06 -0300

    Add tmux plugin manager (tpm)

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc awsrc tmux.conf emacs pryrc )
+files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc awsrc tmux.conf emacs pryrc tmux)
 
 for filename in ${files[@]}
 do
