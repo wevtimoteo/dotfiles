2abf086d6b3107e6896a3aadab6d87c36746768d
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2015-12-07 11:42:34 -0200

    Set marmalade for emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,8 @@
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/"))
+
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (load-theme 'rubytapas t)
 
