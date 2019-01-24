50ab3328302a7cf54dc6c4a37f6c40383247df67
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2019-01-24 00:09:34 -0200

    Replace symlinks when installing

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ files=( ackrc bashrc irbrc alias vim vimrc git-completion.sh gitconfig gitignore
 for filename in ${files[@]}
 do
   [[ -s $HOME/.$filename ]] && rm $HOME/.$filename
-  ln -s $PWD/$filename ~/.$filename
+  ln -sf $PWD/$filename ~/.$filename
 done
 
 sudo ln -fs $PWD/git-edit /usr/local/bin/git-edit
 sudo ln -fs $PWD/capssh /usr/local/bin/capssh
 sudo ln -fs $PWD/capcssh /usr/local/bin/capcssh
-ln -s ~/.vim ~/.config/nvim
+ln -sf ~/.vim ~/.config/nvim
 
 source ~/.bashrc
