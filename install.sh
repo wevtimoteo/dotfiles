e4799f08704c6ca20e00b3987ab94bd4a6119d79
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2019-01-25 14:18:56 -0200

    Introduce tigrc file

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc tmux.conf emacs pryrc tmux hub.bash_completion.sh hyper.js )
+files=( bash_profile irbrc alias vim vimrc git-completion.sh gitconfig gitignore_global gemrc tmux.conf emacs pryrc tmux hub.bash_completion.sh hyper.js tigrc )
 
 for filename in ${files[@]}
 do
   [[ -s $HOME/.$filename ]] && rm $HOME/.$filename
-  ln -s $PWD/$filename ~/.$filename
+  ln -sf $PWD/$filename ~/.$filename
 done
 
-ln -fs $PWD/git-edit /usr/local/bin/git-edit
-ln -fs $PWD/capssh /usr/local/bin/capssh
-ln -fs $PWD/capcssh /usr/local/bin/capcssh
-ln -s ~/.vim ~/.config/nvim
+ln -sf $PWD/git-edit /usr/local/bin/git-edit
+ln -sf $PWD/capssh /usr/local/bin/capssh
+ln -sf $PWD/capcssh /usr/local/bin/capcssh
+ln -sf ~/.vim ~/.config/nvim
 
 dirs=( emacs.d )
 
 for dir in ${dirs[@]}
 do
   [[ -s $HOME/.$dir ]] && rm $HOME/.$dir
-  ln -s $PWD/$dir ~/.$dir
+  ln -sf $PWD/$dir ~/.$dir
 done
 
 source ~/.bash_profile
