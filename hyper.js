422d9e98652ba0af8c4f0a33c63facae43aca73e
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2019-06-18 14:55:56 -0300

    Decrease Hyper terminal font size

--- hyper.js
+++ hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 17,
+    fontSize: 14,
 
     // font family with optional fallbacks
     fontFamily: 'Ubuntu Mono derivative Powerline, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
