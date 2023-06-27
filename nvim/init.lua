d198f8dd8fc5349e3a8c8186ffc953a736990bdd
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-06-27 14:54:10 -0300

    Configure Copilot for LazyVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,8 @@
 local node_version = vim.fn.systemlist("asdf list nodejs | tail -1")[1]
 node_version = string.gsub(node_version, "*", "") -- Remove leading *
 
-vim.g.neovim_node_host_prog = vim.fn.trim(vim.fn.system("asdf where nodejs " .. node_version))
+local node_bin_path = vim.fn.trim(vim.fn.system("asdf where nodejs " .. node_version)) .. "/bin"
+vim.g.neovim_node_host_prog = node_bin_path .. "/node"
 
 -- Loads LazyVim
 require("config.lazy")
---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      copilot_node_command = vim.g.neovim_node_host_prog,
+    },
+  },
+}
