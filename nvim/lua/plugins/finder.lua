e2a48240fb5ea1e72bdca7148db1cc9299a38354
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-06-27 15:04:36 -0300

    Set nvim-spectre plugin for searching in entire project

--- nvim/lua/plugins/finder.lua
+++ nvim/lua/plugins/finder.lua
@@ -39,4 +39,13 @@ return {
       end
     end,
   },
+  {
+    "nvim-pack/nvim-spectre",
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+    },
+  },
 }
