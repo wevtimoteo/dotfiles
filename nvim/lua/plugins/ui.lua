617ec449a247e95ef7c874a4e2eb5dfb33e25229
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-06-28 19:04:56 -0300

    Configure nvim-highlight-colors plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,7 +3,14 @@ return {
     "brenoprata10/nvim-highlight-colors",
     config = function()
       local highlight_colors = require("nvim-highlight-colors")
+
       highlight_colors.turnOn()
+
+      highlight_colors.setup({
+        render = "first_column",
+        enable_named_colors = true,
+        enable_tailwind = false,
+      })
     end,
   },
   {
