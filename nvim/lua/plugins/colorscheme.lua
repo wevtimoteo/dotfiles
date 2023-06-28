835f97a06f439d0df45fce10e9298d59818f3733
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-06-28 15:13:44 -0300

    Customize fluoromachine appearance for Telescope

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,13 +4,40 @@ return {
     config = function()
       local fm = require("fluoromachine")
 
+      local custom_theme = {
+        fg = "#7C0097",
+        bg = "#FAE5FF",
+      }
+
+      local custom_title_theme = {
+        bg = "#7C0097",
+        fg = "#FAE5FF",
+      }
+
       fm.setup({
         glow = true,
         theme = "delta",
         brightness = 0.07,
+        overrides = {
+          TelescopePromptTitle = custom_title_theme,
+          TelescopePreviewTitle = custom_title_theme,
+          TelescopeResultsTitle = custom_title_theme,
+          TelescopeResultsBorder = custom_theme,
+          TelescopeResultsNormal = custom_theme,
+        },
       })
 
+      fm.colors.selection = "#7d229c"
+
       vim.cmd.colorscheme("fluoromachine")
+
+      local lualine = require("lualine")
+
+      lualine.setup({
+        options = {
+          theme = "fluoromachine",
+        },
+      })
     end,
   },
   {
