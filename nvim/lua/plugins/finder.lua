ce88ef22b3958d41b1d73e2b98610f64f83ad254
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-06-28 19:30:05 -0300

    Fix overwrite flash.Char.setup to remove `;` character

--- nvim/lua/plugins/finder.lua
+++ nvim/lua/plugins/finder.lua
@@ -18,30 +18,53 @@ return {
   },
   {
     "folke/flash.nvim",
-    config = function()
+    setup = function()
       local flash = require("flash")
 
-      flash.setup = function()
-        -- Removes `;` from default setup function
-        for _, key in ipairs({ "f", "F", "t", "T", "," }) do
-          if vim.tbl_contains(Config.modes.char.keys, key) then
-            vim.keymap.set({ "n", "x", "o" }, key, function()
-              if Repeat.is_repeat then
-                M.jumping = true
-                M.state:jump({ count = vim.v.count1 })
-                M.state:show()
-                vim.schedule(function()
-                  M.jumping = false
-                end)
-              else
-                M.jump(key)
-              end
-            end, {
-              silent = true,
-            })
-          end
+      local function overrideCharSetup()
+        flash.Repeat.setup()
+
+        -- Override keys list (to remove `;`, since I'm using it on normal mode)
+        local keys = { "f", "F", "t", "T", "," }
+
+        for _, key in ipairs(keys) do
+          vim.keymap.set({ "n", "x", "o" }, key, function()
+            if flash.Repeat.is_repeat then
+              flash.Char.jumping = true
+              flash.Char.state:jump({ count = vim.v.count1 })
+              flash.Char.state:show()
+              vim.schedule(function()
+                flash.Char.jumping = false
+              end)
+            else
+              flash.Char.jump(key)
+            end
+          end, {
+            silent = true,
+          })
         end
+
+        vim.api.nvim_create_autocmd({ "BufLeave", "CursorMoved", "InsertEnter", "TextYankPost" }, {
+          group = vim.api.nvim_create_augroup("flash_char", { clear = true }),
+          callback = function(event)
+            local hide = event.event == "InsertEnter"
+              or (event.event == "TextYankPost" and vim.v.operator == "y")
+              or not flash.Char.jumping
+            if hide and flash.Char.state then
+              flash.Char.state:hide()
+            end
+          end,
+        })
+
+        vim.on_key(function(key)
+          if flash.Char.state and key == flash.Util.ESC and vim.fn.mode() == "n" then
+            flash.Char.state:hide()
+          end
+        end)
       end
+
+      -- Override Char.setup with the new function
+      flash.Char.setup = overrideCharSetup
     end,
   },
   {
