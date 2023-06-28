5a0ef305f2d938fbb65b03efe82549d1b3a52e84
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-06-27 22:59:11 -0300

    Configure buffline to work with tabs

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -4,12 +4,20 @@ return {
     config = function()
       require("bufferline").setup({
         options = {
+          mode = "tabs",
           show_buffer_icons = true,
           show_buffer_close_icons = false,
           show_tab_indicators = true,
           enforce_regular_tabs = true,
           separator_style = "slant",
-          numbers = "ordinal",
+          numbers = "none",
+          truncate_names = false,
+          diagnostics = "nvim_lsp",
+          color_icons = true,
+          always_show_bufferline = true,
+          hover = {
+            enabled = false,
+          },
         },
       })
     end,
