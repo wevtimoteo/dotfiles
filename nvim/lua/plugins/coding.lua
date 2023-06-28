5a9aff4cafb847f63b03d5270257c3f7d608cc11
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-06-28 15:19:50 -0300

    Remove editor.lua (organization)

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -3,4 +3,18 @@ return {
     "echasnovski/mini.pairs",
     enabled = false,
   },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
 }
