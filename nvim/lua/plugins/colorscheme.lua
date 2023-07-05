76e1334daf750acee3becbb646a0c1ab8d034047
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-07-05 17:52:38 -0300

    Customize fluoromachine colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,34 +5,48 @@ return {
       local fm = require("fluoromachine")
 
       local custom_theme = {
-        fg = "#7C0097",
-        bg = "#FAE5FF",
-      }
-
-      -- Reverse the colors
-      local custom_title_theme = {
-        bg = custom_theme.fg,
-        fg = custom_theme.bg,
+        default = {
+          fg = "#7C0097",
+          bg = "#FAE5FF",
+        },
+        title = {
+          fg = "#FAE5FF",
+          bg = "#7C0097",
+        },
+        match = {
+          fg = "#DCAAFF",
+          bg = "#7B439C",
+        },
+        search = {
+          fg = "#EED1FF",
+          bg = "#9C429A",
+        },
+        selection = {
+          fg = "#EED1FF",
+          bg = "#6E3273",
+        },
       }
 
       fm.setup({
         glow = true,
         theme = "delta",
-        brightness = 0.05,
+        brightness = 0.07,
         overrides = {
-          TelescopePromptTitle = custom_title_theme,
-          TelescopePreviewTitle = custom_title_theme,
-          TelescopeResultsTitle = custom_title_theme,
-          TelescopeResultsBorder = custom_theme,
-          TelescopeResultsNormal = custom_theme,
-          TelescopeMatching = {
-            bg = "#FF6596",
-            fg = custom_title_theme.fg,
-          },
+          TelescopePromptTitle = custom_theme.title,
+          TelescopePreviewTitle = custom_theme.title,
+          TelescopeResultsTitle = custom_theme.title,
+          TelescopeResultsBorder = custom_theme.default,
+          TelescopeResultsNormal = custom_theme.default,
+          TelescopeMatching = custom_theme.match,
+          TelescopeSelection = custom_theme.selection,
+          Search = custom_theme.search,
+          Visual = custom_theme.selection,
+          illuminatedWord = custom_theme.match,
+          illuminatedCurWord = custom_theme.search,
+          CmpItemAbbrMatch = custom_theme.match,
+          CmpItemAbbrMatchFuzzy = custom_theme.match,
         },
       })
-
-      fm.colors.selection = "#7d229c"
 
       vim.cmd.colorscheme("fluoromachine")
 
