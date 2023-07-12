6f9c434cef2bd6ec4cad0ab605bb9bb50a17218e
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-07-12 15:43:55 -0300

    Include ackrc file

--- install.zsh
+++ install.zsh
@@ -47,7 +47,7 @@ while (( $# > 0 )); do
   esac
 done
 
-files=(zshrc zsh irbrc alias vim gitconfig gitignore_global gemrc tmux.conf tmux tigrc exports)
+files=(zshrc zsh irbrc alias vim gitconfig gitignore_global gemrc tmux.conf tmux tigrc exports ackrc)
 setup_dotfiles "$HOME" yes "${files[@]}"
 
 source "$HOME/.exports"
