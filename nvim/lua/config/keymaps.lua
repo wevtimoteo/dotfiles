006d8f147d7912ea2a69546f1e1999fd39c01f62
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-09-23 12:45:19 -0300

    Remove keymap for selecting inner text (wildfire plugin solves it)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -34,9 +34,6 @@ set_keymap("c", "<C-e>", "<C-r>=expand('%:p:h')<CR>/", { desc = "Fill current di
 -- replace
 set_keymap("n", "<leader>:", ":%s/:(w+)(s*=>s*)/\1: /gc<CR>", { desc = "Replace old Ruby hash syntax to new one" })
 
--- select
-set_keymap("n", "<CR>", 'vi"', { desc = "Select content inside string", silent = false })
-
 -- Set the key mapping for <Up> to increment cursor number
 set_keymap("n", "<Up>", "<C-a>", {
   desc = "Increment cursor number",
