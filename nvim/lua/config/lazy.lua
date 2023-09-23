27c5c1f658acf177d5470e28bd8770b33c3f5f40
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-09-23 12:21:01 -0300

    Disable ui.animate (was causing issues on paste)

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- { import = "lazyvim.plugins.extras.editor.mini-files" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
 
     -- import/override with your plugins
