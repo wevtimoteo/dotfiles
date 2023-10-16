3c8136caaa1b70ab4be1d6495ee6b082ce2662a0
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-10-16 12:29:36 -0300

    Add FormatHTML lua function to wrap tidy-html5

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- FormatHTML through tidy-html5
+function FormatHTML()
+  local orig_pos = vim.fn.getpos(".")
+  local buf_content = vim.fn.getline(1, "$")
+  local formatted_content = vim.fn.systemlist("tidy -q -i -w 0", buf_content)
+  vim.fn.setline(1, formatted_content)
+  vim.fn.setpos(".", orig_pos)
+end
+
 vim.cmd([[
   au BufRead,BufNewFile Podfile set filetype=ruby
   au BufRead,BufNewFile Guardfile set filetype=ruby
