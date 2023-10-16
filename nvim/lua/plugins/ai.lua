2096a4315a1763eca00591cb7da89b4475b9b1a6
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-10-16 12:32:01 -0300

    Setup Copilot for LazyVim

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,8 +1,16 @@
 return {
   {
     "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
     opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
       copilot_node_command = vim.g.neovim_node_host_prog,
     },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
   },
 }
