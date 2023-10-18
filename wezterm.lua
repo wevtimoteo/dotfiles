605bace4e7b6744f3b432f146a6afaa428c90dd3
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-10-18 15:49:47 -0300

    Tweak wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -10,6 +10,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- Updates
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 604800 -- 7 days
+
+-- Performance
+config.webgpu_power_preference = "HighPerformance"
+config.front_end = "WebGpu"
+
 -- UI
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
@@ -19,8 +27,11 @@ config.use_fancy_tab_bar = false
 config.color_scheme = "Catppuccin Macchiato"
 --config.color_scheme = "purplepeter"
 
+-- Fonts
 config.font = wezterm.font("FiraCodeGG Nerd Font")
-config.font_size = 13
+config.font_size = 14
+
+config.anti_alias_custom_block_glyphs = true
 
 -- Notifications
 config.audible_bell = "Disabled"
