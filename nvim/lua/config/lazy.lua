668642cc41b24d8fe8b6140ff29ac1bfaf766d53
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2023-12-29 12:26:27 -0300

    Update lazyvim packages

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,8 +15,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
