3522a747197ff172ee4d2d377eb4acdd5ae20fc3
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2024-01-11 13:52:55 -0300

    Add lspsaga plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -174,4 +174,14 @@ return {
       })
     end,
   },
+  {
+    "nvimdev/lspsaga.nvim",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
