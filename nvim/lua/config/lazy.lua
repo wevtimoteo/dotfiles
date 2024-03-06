b4405e62f0893cb457f79a24956deb131e980508
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2024-03-06 15:17:27 -0300

    Disable CoPilot nvim plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
 
     -- import/override with your plugins
     { import = "plugins" },
