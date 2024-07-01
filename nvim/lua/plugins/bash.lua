9c2f78d90940e9b7e3a31b35750853e8a61291e3
Weverton Timoteo <v-weverton.timoteo@kajabi.com>
2024-07-01 10:20:17 -0300

    Configure bash defaults

---
+++ nvim/lua/plugins/bash.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {
+          indent_style = "space",
+          indent_size = 2,
+        },
+      },
+    },
+  },
+}
