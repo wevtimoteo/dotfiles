8ef069b84aa246eb8fa2fe3253160b5860ec78ba
Weverton Timoteo <weverton@greatquestion.co>
2025-06-10 14:28:06 -0300

    Bump avante.nvim and update config file

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -4,18 +4,20 @@ return {
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     opts = {
-      provider = "groq", -- In this example, use Claude for planning, but you can also use any provider you want.
+      provider = "claude", -- In this example, use Claude for planning, but you can also use any provider you want.
       cursor_applying_provider = "groq", -- In this example, use Groq for applying, but you can also use any provider you want.
       behaviour = {
         enable_cursor_planning_mode = true, -- enable cursor planning mode!
       },
-      vendors = {
+      providers = {
         groq = { -- define groq provider
           __inherited_from = "openai",
           api_key_name = "GROQ_API_KEY",
           endpoint = "https://api.groq.com/openai/v1/",
           model = "llama-3.3-70b-versatile",
-          max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+          extra_request_body = {
+            max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+          },
         },
       },
     },
@@ -33,7 +35,6 @@ return {
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
