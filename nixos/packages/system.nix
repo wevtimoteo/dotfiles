d62fc657c804ddfc762226e47b169556ddc11fd3
Weverton Timoteo <weverton.ct@gmail.com>
2025-09-07 20:43:35 -0300

    Enable generationLimit for NixOS

--- nixos/packages/system.nix
+++ nixos/packages/system.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 
 {
+  # Limit the number of generations to retain
+  system.build.nixosSystem.generationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 5;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+
   environment.systemPackages = with pkgs; [
     curl
     docker
