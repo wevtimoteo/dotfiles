d7ba532dce1c31c98257622f68bf9318f0b19ed3
Weverton Timoteo <wevtimoteo@users.noreply.github.com>
2025-10-12 21:24:14 +0000

    Adjust `install.sh` packages

--- install.sh
+++ install.sh
@@ -4,19 +4,16 @@
 # Install required packages for Codespaces
 sudo apt update
 sudo apt install -y \
-	tmux \
-	zsh \
+    exa \
 	curl \
 	git \
 	ripgrep \
 	fd-find \
 	fzf \
 	bat \
-	neovim \
 	tig \
 	direnv \
 	jq 
-# eza, yazi, antidote, eza, and others may require manual install or are not available in apt
 
 wget "https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb"
 sudo dpkg -i vivid_0.8.0_amd64.deb
