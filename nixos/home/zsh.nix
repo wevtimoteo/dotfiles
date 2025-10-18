cc9a6fcbd3fc61e420906ff5918028cc6b5d796a
Weverton Timoteo <weverton.ct@gmail.com>
2025-10-18 17:19:54 -0300

    Remove NixOS setup

--- nixos/home/zsh.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  programs.zsh = {
-    enable = true;
-    enableAutosourcing = true;
-  }
-}
--- nixos/modules/tmux.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  programs.tmux.enable = true;
-  programs.tmux.extraConfig = ''
-    set -g mouse on
-  '';
-}
